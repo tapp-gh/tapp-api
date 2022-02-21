4538cf3d9ac761cdcccb3f083dede6ec7a7e76ef
Dorcas Laryea <naa1995ad@gmail.com>
2022-02-21 10:28:01 +0000

    update to user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     email { Faker::Internet.email(name: name) }
     name { Faker::Name.name }
     date_of_birth { Faker::Date.birthday(min_age: 18) }
+    is_active { true }
   end
 end
