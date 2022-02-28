553f0426a12f1e19d32cc411d7df8ccda109af07
Dorcas Laryea <naa1995ad@gmail.com>
2022-02-28 18:43:13 +0000

    comment out secure password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :name, presence: true
     validates :date_of_birth, presence: true
-    has_secure_password
+    # has_secure_password
 
     scope :active, -> { where(is_active: true).order(:name) }
 end
