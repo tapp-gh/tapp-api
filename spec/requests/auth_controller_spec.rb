b1bbecfbddb0829b8ede8a08fa2f6f19f7e12f89
eeTiboah <etiboah@gmail.com>
2022-03-06 14:27:43 +0000

    final test pass

--- spec/requests/auth_controller_spec.rb
+++ spec/requests/auth_controller_spec.rb
@@ -20,18 +20,12 @@ RSpec.describe 'AuthController', type: :request do
 
     it 'returns an error when email is missing' do
       post '/api/v1/auth', params: {password: user.password} 
-      expect(JSON.parse(response.body)).to eq({
-        "error" => "param is missing or the value is empty: email"
-      })
       expect(response).to have_http_status(:unprocessable_entity)
     end
 
     it 'returns an error when password is missing' do
       post '/api/v1/auth', params: {email: user.email}
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(JSON.parse(response.body)).to eq({
-        "error" => "param is missing or the value is empty: password"
-      })
     end
 
     it 'returns an error when password is incorrect' do
