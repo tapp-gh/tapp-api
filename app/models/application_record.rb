1295b57724b54ad7c2b0799ff82201ce25279fec
Dorcas Laryea <naa1995ad@gmail.com>
2022-03-09 20:32:36 +0000

    application record error corrected

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  
+
+  before_create { self.is_active = true if self.has_attribute?(:is_active) }
+
   def delete
     self.has_attribute?(:is_active) ? self.update(is_active: false) : super
   end
