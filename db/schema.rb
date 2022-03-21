97c8dbb195d378b1e19f34d3b3d213c9150b9f1f
Dorcas Laryea <naa1995ad@gmail.com>
2022-03-21 21:28:34 +0000

    merge conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_03_01_195507) do
+=======
+ActiveRecord::Schema.define(version: 2022_02_22_155002) do
+>>>>>>> a115ad3 (dockerfile creation for api)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
