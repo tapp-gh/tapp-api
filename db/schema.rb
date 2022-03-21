9318dbe829882c1e0b322ead30dfb1caf2299703
Dorcas Laryea <naa1995ad@gmail.com>
2022-03-21 22:04:51 +0000

    merge conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -1,20 +1,5 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_03_01_195507) do
-=======
-ActiveRecord::Schema.define(version: 2022_02_22_155002) do
->>>>>>> a115ad3 (dockerfile creation for api)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
